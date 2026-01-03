69042d34a48697a04fd3101b61e72752c1269f1f
NarimanOusman <narimanrobo8@gmail.com>
2026-01-03 07:08:24 -0800

    created the home page

---
+++ pages/views.py
@@ -0,0 +1,18 @@
+
+# Create your views here.
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def contact(request):
+    return render(request, 'contact.html')
